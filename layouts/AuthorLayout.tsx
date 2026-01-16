bb065cb286cee84912c2909ea012aaf1bacab591
abdulhakeem <abdulhakeemadefioye@gmail.com>
2026-01-16 01:04:16 -0600

    Centered occupation

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -31,7 +31,7 @@ export default function AuthorLayout({ children, content }: Props) {
               />
             )}
             <h3 className="pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+            <div className="w-full text-center text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
